16fa762122096923cefe18a177945e10fdbdd58d
Thorin Schiffer <thorin@schiffer.pro>
2021-08-13 16:52:47 +0200

    Fixing secret key warning

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -23,7 +23,7 @@ environ.Env.read_env(os.path.join(os.path.dirname(BASE_DIR), '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t^didwc$3$y6gxqg8n(q-oez24yp-rdz46u(bfp41gt7im(yjd'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
